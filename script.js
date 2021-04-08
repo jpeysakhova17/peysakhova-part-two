b66823a0a202f26526913068d30fe5efc89bb8d1
jpeysakhova17 <81653295+jpeysakhova17@users.noreply.github.com>
2021-04-08 17:54:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+Step 1: Need to get the pets name 
+                (petsname)
+step 2: both happiness and energy level start at 0
+step 3: need to loop 6x asking if they:
+    -feed (increases energy by 2)
+    -pet (increases happiness by 1)
+    -walk (increases happiness by 2 but decreases energy by 1)
+step 4: if user chooses walk but energy is at 0: alert - "Not enough energy to enjoy a walk"
+    -if (total <= 0) send alert
+*/
+
+
+
+let petname = prompt("Name your pet");
+
+prompt ("feed, pet, or walk?");
+let energy = ["feed", "pet", "walk"];
+energy [2];
+energy [0];
+energy [-1];
+total += energy;
+let total = 0;
+
+
+let happiness = ["feed", "pet", "walk"];
+happiness [0];
+happiness [1]
+happiness [2]
+
+
+if (energy -= 0)
+
+else {alert = "Not enough energy to enjoy a walk"};
+
+
+
